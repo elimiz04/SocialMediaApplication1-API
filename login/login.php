39d7672da5a1d2ec333bde488fea30d73b75f575
elimiz04 <mizzielisea4@gmail.com>
2024-05-30 00:36:22 +0200

    Adjusted styling of login and sign up pages
    increases size of boxes
    Added labels for username and password and left-aligned them
    added hover effect on button to show a change of colour
    center aligned box

--- login/login.php
+++ login/login.php
@@ -28,7 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         
         echo "Wrong username or password!";
     } else {
-        echo "Wrong username or password!";
+        echo "Please enter some valid information!";
     }
 }
 ?>
@@ -39,19 +39,69 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-</head>
-<body>
     <style type="text/css"> 
         /* CSS styling for form elements */
+        body, html {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #f0f0f0; /* Background color for the entire page */
+        }
+        #text {
+            height: 25px;
+            border-radius: 5px;
+            padding: 4px;
+            border: solid thin #aaa;
+            width: 100%;
+        }
+        #button {
+            padding: 12px;
+            width: 120px;
+            font-size: 16px;
+            color: white;
+            background-color: #337ab7; /* Darker button color */
+            border: none;
+            transition: background-color 0.3s ease; /* Smooth transition on hover */
+        }
+        #button:hover {
+            background-color: #286090; /* Slightly darker color on hover */
+        }
+        #box {
+            background-color: grey;
+            width: 400px;
+            padding: 40px;
+            text-align: center;
+        }
+        #signup-link {
+            font-size: 16px;
+            color: white;
+            text-align: center;
+            display:block;
+            margin-top: 20px;
+        }
+        .form-label {
+            font-size: 16px;
+            text-align: left;
+            font-weight: bold;
+            margin-bottom: 10px;
+            color: white;
+            display: block;
+        }
     </style>
-
+</head>
+<body>
     <div id="box">
         <form method="post">
-            <div style="font-size: 20px; margin: 10px; color: white;">Login</div>
+            <div style="font-size: 24px; margin: 20px; color: white;text-align: center;">Login</div>
+            <label for="user_name" class="form-label">Username:</label>
             <input id="text" type="text" name="user_name"> <br><br>
+            <label for="password" class="form-label">Password:</label>
             <input id="text" type="password" name="password"><br><br>
             <input id="button" type="submit" value="Login"><br><br>
-            <a href="signup.php">Click to Signup</a><br><br>
+            <a href="signup.php" style="color: white;">Click to Signup</a><br><br>
         </form>
     </div>
 </body>
--- login/signup.php
+++ login/signup.php
@@ -40,30 +40,55 @@
 <body>
     <style type= "text/css"> 
         
-        #text{
-
+         /* CSS styling for form elements */
+         body, html {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #f0f0f0; /* Background color for the entire page */
+        }
+         #text {
             height: 25px;
             border-radius: 5px;
             padding: 4px;
             border: solid thin #aaa;
             width: 100%;
         }
-
-        #button{
-
-            padding: 10px;
-            width: 100px;
+        #button {
+            padding: 12px;
+            width: 120px;
+            font-size: 16px;
             color: white;
-            background-color: lightblue;
+            background-color: #337ab7; /* Darker button color */
             border: none;
+            transition: background-color 0.3s ease; /* Smooth transition on hover */
         }
-
-        #box{
-
+        #button:hover {
+            background-color: #286090; /* Slightly darker color on hover */
+        }
+        #box {
             background-color: grey;
-            margin: auto;
-            width: 300px;
-            padding: 20px;
+            width: 400px;
+            padding: 40px;
+            text-align: center;
+        }
+        #signup-link {
+            font-size: 16px;
+            color: white;
+            text-align: center;
+            display:block;
+            margin-top: 20px;
+        }
+        .form-label {
+            font-size: 16px;
+            text-align: left;
+            font-weight: bold;
+            margin-bottom: 10px;
+            color: white;
+            display: block;
         }
 
     </style>
@@ -71,14 +96,15 @@
     <div id = "box">
         <form method="post">
 
-            <div style=" font-size: 20px;margin: 10px;color: white;">Signup</div>
-
+            <div style=" font-size: 24px;margin: 20px;color: white; text-align: center;">Signup</div>
+            <label for="user_name" class="form-label">Username:</label>
             <input id="text" type="text" name="user_name"> <br><br>
+            <label for="password" class="form-label">Password:</label>
             <input id="text" type="password" name="password"><br><br>
 
             <input id="button" type="submit" value="Signup"><br><br>
 
-            <a href="login.php">Click to Login</a><br><br>
+            <a href="login.php" style="color: white";>Click to Login</a><br><br>
         </form>
     </div>
 </body>
