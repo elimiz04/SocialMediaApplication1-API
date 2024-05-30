f3f11da760f8c246e90c91f565d4d658827d5cb0
elimiz04 <mizzielisea4@gmail.com>
2024-05-30 22:18:12 +0200

    changed my sql database

--- login/connection.php
+++ login/connection.php
@@ -3,7 +3,7 @@
 $dbhost = "localhost"; 
 $dbuser = "root"; 
 $dbpass = ""; 
-$dbname = "login_sample_db"; 
+$dbname = "social_media"; 
 
 if(!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)){
 
