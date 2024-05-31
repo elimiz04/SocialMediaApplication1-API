ac8bc37101a2dd9ff07a1c491e58964a5fdbecc4
elimiz04 <mizzielisea4@gmail.com>
2024-05-31 12:54:50 +0200

    Edit post and delete post where created to handle post funtion
    Users are able to edit post

--- login/index.php
+++ login/index.php
@@ -94,13 +94,26 @@ $posts_result = mysqli_query($con, $posts_query);
         </form>
         <h2>Posts</h2>
         <?php
-        if (mysqli_num_rows($posts_result) > 0) {
             while ($post = mysqli_fetch_assoc($posts_result)) {
                 echo "<div class='post'><b>" . $post['username'] . ":</b><br>" . $post['content'] . "<br><small>" . $post['created_at'] . "</small></div>";
+                //comment form
+                echo"
+                <form method ='post' action= 'create_comment.php'>
+                    <textarea name 'content' placeholder= 'Add a comment...' style= 'width:100%; height: 50px; border-radius:5px; padding:10px; margin: 10px 0 ; ' ></textarea>
+                    <input type='hidden' name ='post_id' value='" . $post['post_id']."'>
+                    <input id ='button' type= 'submit' value='Comment'>
+                </form>";
+
+                //Display comments
+                $comments_query = "SELECT comments.*, users.username FROM comments JOIN users ON comments.user_id = users.user_id WHERE comments.post_id = '".$post['post_id']."' ORDER BY comments.created_at ASC";
+                $comments_result = mysqli_query($con, $comments_query);
+                if(mysqli_num_rows($comments_result) > 0){
+                    while ($comment = mysqli_fetch_assoc($comment_result)){
+                        echo "<div class = 'comment'><b>". $comment['username'] . ":</b><br>" . $comment['content']. "<br> <small>". $comment['created_at']. "</small></div>";
+                    }
+                }
             }
-        } else {
-            echo "<div class='post'>No posts available</div>";
-        }
+      
         ?>
     </div>
 </body>
